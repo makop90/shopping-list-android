1830060a0f3a65d25fd197f077fe66cf75b32a3a
Mateusz Kopec <Mateusz Kopec>
2017-03-30 13:27:22 +0200

    lecture 9 exercies 2 finished - exercies 3 missing

--- app/src/main/java/org/projects/shoppinglist/MainActivity.java
+++ app/src/main/java/org/projects/shoppinglist/MainActivity.java
@@ -1,9 +1,9 @@
 package org.projects.shoppinglist;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,41 +12,29 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyDialogFragment.OnPositiveListener {
 
-
-    ArrayAdapter<String> adapter;
-    ListView listView;
-    ArrayList<String> bag = new ArrayList<String>();
-
-    public ArrayAdapter getMyAdapter() {
-        return adapter;
-    }
-
+    public ArrayAdapter<Product> adapter;
+    public ListView listView;
+    public ArrayList<Product> bag = new ArrayList<Product>();
+    public static MyDialogFragment dialog;
+    public static Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.context = MainActivity.this;
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        //getting our listiew - you can check the ID in the xml to see that it
-        //is indeed specified as "list"
         listView = (ListView) findViewById(R.id.list);
-        //here we create a new adapter linking the bag and the
-        //listview
-        adapter = new ArrayAdapter<String>(this,
+        adapter = new ArrayAdapter<Product>(this,
                 android.R.layout.simple_list_item_checked, bag);
-
-        //setting the adapter on the listview
         listView.setAdapter(adapter);
-        //here we set the choice mode - meaning in this case we can
-        //only select one item at a time.
         listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
         Button addButton = (Button) findViewById(R.id.addButton);
@@ -55,12 +43,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText textField = (EditText) findViewById(R.id.name);
                 EditText textField2 = (EditText) findViewById(R.id.quantity);
-                String text = textField2.getText().toString() + " " + textField.getText().toString();
-                //   bag.add(text);
-                //The next line is needed in order to say to the ListView
-                //that the data has changed - we have added stuff now!
-                bag.add(text);
-                getMyAdapter().notifyDataSetChanged();
+                Product p = new Product(textField.getText().toString(), Integer.parseInt(textField2.getText().toString()));
+                bag.add(p);
+                adapter.notifyDataSetChanged();
             }
         });
 
@@ -80,74 +65,46 @@ public class MainActivity extends AppCompatActivity {
                     bag.remove(toDelete.get(i) - i);
                 }
                 listView.setAdapter(adapter);
-                getMyAdapter().notifyDataSetChanged();
+                adapter.notifyDataSetChanged();
             }
         });
-
-        Button clearButton = (Button) findViewById(R.id.clearButton);
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                adapter.clear();
-                listView.setAdapter(adapter);
-                getMyAdapter().notifyDataSetChanged();
-            }
-        });
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings: {
+                return true;
+            }
+            case R.id.item_delete: {
+                dialog = new MyDialog();
+                dialog.show(getFragmentManager(), "MyFragment");
+            }
         }
-
         return super.onOptionsItemSelected(item);
     }
-
-    //This method is called before our activity is destoryed
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        //ALWAYS CALL THE SUPER METHOD - To be nice!
         super.onSaveInstanceState(outState);
-        /* Here we put code now to save the state */
-        outState.putStringArrayList("savedBag", bag);
-
+        outState.putParcelableArrayList("savedBag", bag);
         SparseBooleanArray checked = listView.getCheckedItemPositions();
-
         outState.putParcelable("checkedItems", new SparseBooleanArrayParcelable(checked));
     }
 
-    //this is called when our activity is recreated, but
-    //AFTER our onCreate method has been called
-    //EXTREMELY IMPORTANT DETAIL
     @Override
     protected void onRestoreInstanceState(Bundle savedState) {
-        //MOST UI elements will automatically store the information
-        //if we call the super.onRestoreInstaceState
-        //but other data will be lost.
         super.onRestoreInstanceState(savedState);
-		/*Here we restore any state */
-        //in the line below, notice key value matches the key from onSaved
-        //this is of course EXTREMELY IMPORTANT
-        this.bag = savedState.getStringArrayList("savedBag");
-        adapter = new ArrayAdapter<String>(this,
+        this.bag = savedState.getParcelableArrayList("savedBag");
+        adapter = new ArrayAdapter<Product>(this,
                 android.R.layout.simple_list_item_checked, bag);
         listView.setAdapter(adapter);
-        SparseBooleanArray checked = (SparseBooleanArray) savedState.getParcelable("checkedItems");
+        SparseBooleanArray checked = savedState.getParcelable("checkedItems");
         for (int i = 0; i < checked.size(); i++) {
             int key = checked.keyAt(i);
             if (checked.get(key)) {
@@ -156,4 +113,22 @@ public class MainActivity extends AppCompatActivity {
         }
         adapter.notifyDataSetChanged();
     }
+
+    @Override
+    public void onPositiveClicked() {
+        adapter.clear();
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    public static class MyDialog extends MyDialogFragment {
+        @Override
+        protected void negativeClick() {
+            Toast toast = Toast.makeText(context,
+                    "Canceled", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+
 }
---
+++ app/src/main/java/org/projects/shoppinglist/MyDialogFragment.java
@@ -0,0 +1,66 @@
+package org.projects.shoppinglist;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class MyDialogFragment extends DialogFragment {
+
+    public MyDialogFragment(){
+
+    }
+
+    OnPositiveListener mCallback;
+
+    public interface OnPositiveListener {
+        void onPositiveClicked();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mCallback = (OnPositiveListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnPositiveListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(
+                getActivity());
+        alert.setTitle(R.string.confirmation);
+        alert.setMessage(R.string.areYouSure);
+        alert.setPositiveButton(R.string.yes, pListener);
+        alert.setNegativeButton(R.string.no, nListener);
+
+        return alert.create();
+    }
+
+    DialogInterface.OnClickListener pListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface arg0, int arg1) {
+            positiveClick();
+        }
+    };
+
+    DialogInterface.OnClickListener nListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface arg0, int arg1) {
+            negativeClick();
+        }
+    };
+    protected void positiveClick()
+    {
+        mCallback.onPositiveClicked();
+    }
+
+    protected void negativeClick()
+    {
+    }
+}
